888afbb0eccecf4794149785af9e2538be439f05
rvaccarim <67932128+rvaccarim@users.noreply.github.com>
2021-01-04 11:16:42 -0500

    Removed imports and formatted comments

--- src/decoder.py
+++ src/decoder.py
@@ -1,6 +1,4 @@
 import png
-from os import listdir
-from os.path import isfile, join
 from enum import Enum
 
 # https://pico-8.fandom.com/wiki/P8PNGFileFormat
@@ -63,7 +61,8 @@ def get_version(hidden_data):
 
 
 def get_code_plaintext(hidden_data):
-    # the code is stored as plaintext (ASCII), up to the first null byte
+    # the code is stored as plaintext (ASCII), up to the first
+    # null byte
     code = []
     code_pos = 0x4300
 
@@ -79,10 +78,13 @@ def get_code_plaintext(hidden_data):
 
 
 def get_code_oldcompression(hidden_data):
-    CHAR_TABLE = ' \n 0123456789abcdefghijklmnopqrstuvwxyz!#%(){}[]<>+=/*:;.,~_'
+    CHAR_TABLE = \
+        ' \n 0123456789abcdefghijklmnopqrstuvwxyz!#%(){}[]<>+=/*:;.,~_'
 
-    # bytes 0x4304-0x4305 are the length of the decompressed code, stored MSB first.
-    decompressed_length = (hidden_data[0x4304] << 8) | hidden_data[0x4305]
+    # bytes 0x4304-0x4305 are the length of the decompressed code,
+    # stored MSB first.
+    decompressed_length = (hidden_data[0x4304] << 8) | \
+                          hidden_data[0x4305]
 
     # The next two bytes (0x4306-0x4307) are always zero.
     assert hidden_data[0x4306] == 0
@@ -95,7 +97,8 @@ def get_code_oldcompression(hidden_data):
         curr_byte = hidden_data[code_pos]
 
         if curr_byte == 0x00:
-            # 0x00: Copy the next byte directly to the output stream.
+            # 0x00: Copy the next byte directly to the output
+            # stream.
             code.append(chr(hidden_data[code_pos + 1]))
             code_pos += 2
 
@@ -105,12 +108,14 @@ def get_code_oldcompression(hidden_data):
             code_pos += 1
 
         else:
-            # 0x3c-0xff: Calculate an offset and length from this byte and the next byte, then copy those bytes from
-            # what has already been emitted. In other words, go back "offset" characters in the output stream,
-            # copy "length" characters, then paste them to the end of the output stream.
+            # 0x3c-0xff: Calculate an offset and length from this byte
+            # and the next byte, then copy those bytes from what has
+            # already been emitted. In other words, go back "offset"
+            # characters in the output stream, copy "length" characters,
+            # then paste them to the end of the output stream.
             next_byte = hidden_data[code_pos + 1]
 
-            # according to the spec
+            # this magic stuff comes from the format specification
             offset = (curr_byte - 0x3c) * 16 + (next_byte & 0xf)
             index = len(code) - offset
             length = (next_byte >> 4) + 2
@@ -120,12 +125,12 @@ def get_code_oldcompression(hidden_data):
                     b = code[index + i]
                     code.append(b)
             except IndexError as e:
-                return f"ERROR DECODING\noffset={offset} length={length}\n\n" + "".join(code)
+                return f"ERROR DECODING\noffset={offset} length={length}\n\n" \
+                       + "".join(code)
 
             code_pos += 2
 
     return "".join(code)
-
 
 
 def read_bit():
@@ -148,22 +153,27 @@ def get_code_newcompression(hidden_data):
     global stream_pos, stream_str
     code_str = ""
 
-    # bytes 0x4304-0x4305 are the length of the decompressed code, stored MSB first.
-    decompressed_code_length = (hidden_data[0x4304] << 8) | hidden_data[0x4305]
-
-    # The next two bytes (0x4306-0x4307) are the length of the compressed 1_expected + 8 for this 8-byte header,
+    # bytes 0x4304-0x4305 are the length of the decompressed code,
     # stored MSB first.
-    compressed_data_length = hidden_data[0x4306] << 8 | hidden_data[0x4307]
+    decompressed_code_length = (hidden_data[0x4304] << 8) \
+                               | hidden_data[0x4305]
 
-    # The decompression algorithm maintains a "move-to-front" mapping of the 256 possible bytes.
-    # Initially, each of the 256 possible bytes maps to itself.
+    # The next two bytes (0x4306-0x4307) are the length of the
+    # compressed data + 8 for this 8-byte header, stored MSB first.
+    compressed_data_length = hidden_data[0x4306] << 8 \
+                             | hidden_data[0x4307]
+
+    # The decompression algorithm maintains a "move-to-front" mapping
+    # of the 256 possible bytes. Initially, each of the 256 possible
+    # bytes maps to itself.
     move_to_front = []
     for i in range(256):
         move_to_front.append(i)
 
-    # The decompression algorithm processes the compressed 1_expected bit by bit - going from LSB to MSB of each
-    # byte - until the 1_expected length of decompressed characters has been emitted.
-    # We create a string with all bytes inverted to simulate the decoding stream
+    # The decompression algorithm processes the compressed data bit
+    # by bit - going from LSB to MSB of each byte - until the data
+    # length of decompressed characters has been emitted. We create
+    # a string with all bytes inverted to simulate the decoding stream
     stream = []
     code_pos = 0x4308
     while code_pos < 0x8000:
@@ -179,13 +189,18 @@ def get_code_newcompression(hidden_data):
         # log: print(f"stream_pos={stream_pos}, starting loop")
         # log: print(f"next_24_bits={stream_str[stream_pos: stream_pos + 24]}")
 
-        # Each group of bits starts with a single header bit, specifying the group's type.
+        # Each group of bits starts with a single header bit,
+        # specifying the group's type.
         header = read_bit()
         # print(f"stream_pos={stream_pos}, header={header}")
 
+        if code_str.endswith('split"'):
+            xxx = 0
+
         if header == "1":
             # header bit = 1 -> get a character from the index
-            # these values and bit manipulations are documented in the P8.PNG spec
+            # these values and bit manipulations are documented in
+            # the P8.PNG spec
             unary = 0
             while read_bit() == "1":
                 unary += 1
@@ -209,7 +224,8 @@ def get_code_newcompression(hidden_data):
 
         else:
             # header bit = 2 -> copy/paste a segment
-            # these values and bit manipulations are documented in the P8.PNG spec
+            # these values and bit manipulations are documented
+            # in the P8.PNG spec
             # print(f"reading control bits")
             if read_bit() == "1":
                 if read_bit() == "1":
@@ -235,10 +251,10 @@ def get_code_newcompression(hidden_data):
                     break
             # print(f"length={length}")
 
-            # Then we go back "offset" characters in the output stream, and copy "length" characters to the end of
-            # the output stream.
-            # "length" may be larger than "offset", in which case we effectively repeat a pattern
-            # of "offset" characters.
+            # Then we go back "offset" characters in the output stream,
+            # and copy "length" characters to the end of the output stream.
+            # "length" may be larger than "offset", in which case we
+            # effectively repeat a pattern of "offset" characters.
             if offset_backwards > len(code_str):
                 err_str = f"ERROR DECODING\nback_offset={offset_backwards} len code_str={len(code_str)}\n\n"
                 print(err_str)
@@ -264,10 +280,6 @@ def get_code_newcompression(hidden_data):
 
 def repeat_to_length(string_to_expand, length):
     return (string_to_expand * (int(length/len(string_to_expand))+1))[:length]
-
-
-def is_bit_set(value, bit_position):
-    return ((value >> bit_position) & 0b_0000_0001) == 1
 
 
 def extract_code(filename):
